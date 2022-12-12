facd6c98feb0f56c876a9d51ce42fc2b9a4a54bf
Mateo Alganaras <tutealga@gmail.com>
2022-12-12 16:10:17 -0300

    Arreglos

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,8 +1,8 @@
 const data = [
-    {skills:['React','JavaScript','Bootstrap'],title:'Football team', img: require('./assets/surrarifc.jpg'), demo:'https://tutealga.github.io/surrarifc/', github:'https://github.com/Tutealga/surrarifc'},
-    {skills:['WordPress','HTML','CSS'],title:'Butcher shop', img: require('./assets/elorigen.jpg'), demo:'https://carniceriaelorigen.com/'},
+    {skills:['React','JavaScript','Bootstrap'],title:'Football team website', img: require('./assets/surrarifc.jpg'), demo:'https://tutealga.github.io/surrarifc/', github:'https://github.com/Tutealga/surrarifc'},
+    {skills:['WordPress','HTML','CSS'],title:'Butcher shop ecommerce', img: require('./assets/elorigen.jpg'), demo:'https://carniceriaelorigen.com/'},
     {skills:['JavaScript','Bootstrap','API'],title:'Loans simulator', img: require('./assets/simulador.jpg'), demo:'https://tutealga.github.io/simuladorinteractivo/', github:'https://github.com/Tutealga/simuladorinteractivo'},
-    {skills:['HTML','CSS'],title:'Meat processing plant', img: require('./assets/mediavillamark.jpg'), demo:'https://tutealga.github.io/mediavillamark/', github:'https://github.com/Tutealga/mediavillamark'},
+    {skills:['HTML','CSS'],title:'Meat processing plant website', img: require('./assets/mediavillamark.jpg'), demo:'https://tutealga.github.io/mediavillamark/', github:'https://github.com/Tutealga/mediavillamark'},
 ]
 
 export default data;
